59daaefe4fbec3a3e5ec4e9e8b2ef76f8eba63b6
bagooess <imanbagus16@gmail.com>
2024-11-26 17:19:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import math
 st.title("Kalkulator :rainbow[Volume Tabung]\n by bagooess :sunglasses:")
 
 r = st.number_input("Masukan Jari- Jari (cm)", 0)
-r = st.number_input("Masukan Tinggi (cm)", 0)
+t = st.number_input("Masukan Tinggi (cm)", 0)
 
 if st.button("Hitung Volume", type="primary"):
   v = math.pi*(r**2)*t
