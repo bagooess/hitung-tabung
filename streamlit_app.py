7696a7740d5e4da65293a073566e09aad33c89f9
bagooess <imanbagus16@gmail.com>
2024-11-26 17:09:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.title("Kalkulator :rainbow[Volume Tabung]\n by bagooess :sunglasses:")
+
+r = st.number_input("Masukan Jari- Jari (cm)", 0)
+r = st.number_input("Masukan Tinggi (cm)", 0)
